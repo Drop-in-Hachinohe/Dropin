3df32f5ad5bff483751093545dc9fbb3f788a6e5
YuukiIshibashi <robopowerful_vv@yahoo.co.jp>
2023-06-24 14:02:18 +0900

    予約復活

--- src/views/GuestHouse/Home.vue
+++ src/views/GuestHouse/Home.vue
@@ -1,82 +1,107 @@
 <script setup>
-import { ref } from 'vue'
-import Api from '../../api/api.js'
-import NewsList from '../../components/NewsList.vue'
-import { Carousel, Navigation, Slide } from 'vue3-carousel'
-import 'vue3-carousel/dist/carousel.css'
-
+import { ref } from "vue";
+import Api from "../../api/api.js";
+import NewsList from "../../components/NewsList.vue";
+import { Carousel, Navigation, Pagination, Slide } from "vue3-carousel";
+import "vue3-carousel/dist/carousel.css";
 
 const settings = {
   itemsToShow: 1,
-  snapAlign: 'start',
-}
+  snapAlign: "start",
+};
 const breakpoints = {
   // 700px and up
   640: {
     itemsToShow: 1.25,
-    snapAlign: 'start',
+    snapAlign: "start",
   },
   // 1024 and up
   768: {
     itemsToShow: 3,
-    snapAlign: 'center',
+    snapAlign: "center",
   },
-}
+};
 const features = [
-  { 
-    imageUrl: '/images/GH/feature_01.jpg',
-    title: "海まで徒歩10分"
+  {
+    imageUrl: "/images/GH/feature_01.jpg",
+    title: "海まで徒歩10分",
   },
-  { 
-    imageUrl: '/images/GH/feature_02.jpg',
-    title: "音楽も楽しむ"
+  {
+    imageUrl: "/images/GH/feature_02.jpg",
+    title: "音楽も楽しむ",
   },
-  { 
-    imageUrl: '/images/GH/feature_03.jpg',
-    title: "1日1組のみ"
-  }
-]
+  {
+    imageUrl: "/images/GH/feature_03.jpg",
+    title: "1日1組のみ",
+  },
+];
 const facilities = [
-  { 
-    imageUrl: '/images/GH/facility_01.jpg',
+  {
+    imageUrl: "/images/GH/facility_01.jpg",
     title: "1日1組のみのお部屋",
     class: "facility_content_01",
     content: `1階と2階に一つずつ宿泊ルームがあります男女混合のエンジニアチームでもお部屋を分けて合宿できます。（最大４名様）。
       1階のお部屋にはトゥルースリーパーのマットレスを１台用意しております。
-      `
+      `,
   },
-]
+];
 const baths = [
-  { 
-    imageUrl: '/images/GH/bath_01.jpg',
+  {
+    imageUrl: "/images/GH/bath_01.jpg",
   },
-  { 
-    imageUrl: '/images/GH/bath_02.jpg',
+  {
+    imageUrl: "/images/GH/bath_02.jpg",
   },
-  { 
-    imageUrl: '/images/GH/wc.jpg',
-  }
-]
+  {
+    imageUrl: "/images/GH/wc.jpg",
+  },
+];
 const developmentRoomImages = [
-  { 
-    imageUrl: '/images/GH/developmentRoom_01.jpg',
+  {
+    imageUrl: "/images/GH/developmentRoom_01.jpg",
   },
-  { 
-    imageUrl: '/images/GH/developmentRoom_02.jpg',
+  {
+    imageUrl: "/images/GH/developmentRoom_02.jpg",
   },
-  { 
-    imageUrl: '/images/GH/developmentRoom_03.jpg',
+  {
+    imageUrl: "/images/GH/developmentRoom_03.jpg",
   },
-  { 
-    imageUrl: '/images/GH/developmentRoom_04.jpg',
+  {
+    imageUrl: "/images/GH/developmentRoom_04.jpg",
   },
-  { 
-    imageUrl: '/images/GH/developmentRoom_05.jpg',
-  }
-]
+  {
+    imageUrl: "/images/GH/developmentRoom_05.jpg",
+  },
+  {
+    imageUrl: "/images/GH/developmentRoom_06.jpg",
+  },
+  {
+    imageUrl: "/images/GH/developmentRoom_07.jpg",
+  },
+  {
+    imageUrl: "/images/GH/developmentRoom_08.jpg",
+  },
+  {
+    imageUrl: "/images/GH/developmentRoom_09.jpg",
+  },
+];
+const guestRoomImages = [
+  {
+    imageUrl: "/images/GH/guestRoom_01.jpg",
+  },
+  {
+    imageUrl: "/images/GH/guestRoom_02.jpg",
+  },
+  {
+    imageUrl: "/images/GH/guestRoom_03.jpg",
+  },
+  {
+    imageUrl: "/images/GH/guestRoom_04.jpg",
+  },
+];
 const news = ref([]);
 
-Api.fetchNewsIndex('guest_house_top').then((data) => {
+Api.fetchNewsIndex("guest_house_top").then((data) => {
   news.value = data.contents;
 });
 </script>
@@ -85,17 +110,17 @@ Api.fetchNewsIndex('guest_house_top').then((data) => {
   <main>
     <div class="kv">
       <div class="logo">
-        <img src="/images/GH/Dropin_logo.png" class="logoImg"/>
-        <h1 class="text-white text-base text-center">
-          つくるひとのためのいえ
-        </h1>
+        <img src="/images/GH/Dropin_logo.png" class="logoImg" />
+        <h1 class="text-white text-base text-center">つくるひとのためのいえ</h1>
       </div>
     </div>
     <section class="section mt-10">
-      <h2 class="text-2xl text-center font-bold">Drop inは、開発合宿・ワーケーション特化型ゲストハウスです</h2>
+      <h2 class="text-2xl text-center font-bold">
+        Drop inは、開発合宿・ワーケーション特化型ゲストハウスです
+      </h2>
       <p class="text-base mt-10">
-        何にも邪魔されず、目の前の作りたいものに没頭できる環境を目指しています。 
-        宿泊利用は1日1組様のためオンラインミーティングも可能です。(WiFiもご自由にご利用ください。）<br>
+        何にも邪魔されず、目の前の作りたいものに没頭できる環境を目指しています。
+        宿泊利用は1日1組様のためオンラインミーティングも可能です。(WiFiもご自由にご利用ください。）<br />
         デジタルではないモノ作りも応援しています。なんでもご相談ください。
       </p>
       <ul class="mt-20 feature">
@@ -105,38 +130,65 @@ Api.fetchNewsIndex('guest_house_top').then((data) => {
               <p class="text-center font-bold text-teal-600 text-base">
                 {{ feature.title }}
               </p>
-              <img :src="feature.imageUrl" class="rounded-full aspect-square mt-5 md:p-2" />
+              <img
+                :src="feature.imageUrl"
+                class="rounded-full aspect-square mt-5 md:p-2"
+              />
             </li>
           </Slide>
           <template #addons>
             <Navigation />
+            <Pagination />
           </template>
         </Carousel>
       </ul>
     </section>
-    <section id="facilities">
-      <h2 class="text-slate-700 text-4xl text-center">ROOM</h2>
-      <p class="text-base text-slate-700　text-center">宿泊ルーム</p>
-      <ul class="sm:mt-16">
-        <li v-for="(facility, index) in facilities" :key="index" class="facility mt-10">
-          <div class="facility_content sm:w-6/12 md:w-5/12 lg:w-4/12 py-6 px-5" :class="facility.class">
-            <h3 class="font-bold text-lg facility_title">{{ facility.title }}</h3>
-            <p class="text-sm mt-4 whitespace-pre-line">
-              {{ facility.content }}
-            </p>
-          </div>
-          <img :src="facility.imageUrl" class="facility_img"/>
-        </li>
-      </ul>
-    </section>
+
     <section class="section mt-20 bg-slate-100">
-      <h2 class="text-slate-700 text-4xl text-center">DEVELOPMENT SPACE</h2>
-      <p class="text-base text-slate-700　text-center">開発スペース</p>
+      <h2 class="text-slate-700 text-4xl text-center">ROOM</h2>
+      <p class="text-base text-slate-700 text-center">宿泊ルーム</p>
       <p class="text-base mt-10">
-        高速ひかりWifiを導入しております。ご自由にご利用ください。疲れた時にはソファで一休みできます∩(ΦωΦ)∩<br>
+        メインの宿泊ルームは1階奥のお部屋です。
+        マットレスベッド２台にロフトベットが１台あります。それとは別に布団が1組ありますので、最大4名様までご宿泊いただけます。<br />
+        収納の中にミニ冷蔵庫があります
+      </p>
+      <div class="mt-16">
+        <Carousel :settings="settings" :breakpoints="breakpoints">
+          <Slide v-for="(item, index) in guestRoomImages" :key="index">
+            <div class="carousel__item">
+              <img :src="item.imageUrl" class="aspect-square" />
+            </div>
+          </Slide>
+          <template #addons>
+            <Navigation />
+            <Pagination />
+          </template>
+        </Carousel>
+        <div class="text-sm mt-8">
+          <p class="font-bold">
+            開発スペースを使って様々なイベントを開催しています。よろしければごらんください!
+          </p>
+          <a
+            href="https://coderdojo-hachinohe.aomori.jp/posts/43109806"
+            target="_blank"
+            class="text-teal-600 underline"
+          >
+            海が見えるゲストハウスで美味しいコーヒーを飲みながら作品作り！第20回CoderDojo八戸
+          </a>
+        </div>
+      </div>
+    </section>
+
+    <section class="section mt-20">
+      <h2 class="text-slate-700 text-4xl text-center">DEVELOPMENT SPACE</h2>
+      <p class="text-base text-slate-700 text-center">開発スペース</p>
+      <p class="text-base mt-10">
+        高速ひかりWifiを導入しております。ご自由にご利用ください。疲れた時にはソファで一休みできます∩(ΦωΦ)∩<br />
         ランニングマシーンもありますので、体を動かしたい時はスニーカーも一緒にお持ちくださいね〜！
       </p>
-      <p class="text-sm mt-6">※開発スペースは宿泊の方以外の方もご利用されている場合がありますm(｡_｡；))m</p>
+      <p class="text-sm mt-6">
+        ※開発スペースは宿泊の方以外の方もご利用されている場合がありますm(｡_｡；))m
+      </p>
       <div class="mt-16">
         <Carousel :settings="settings" :breakpoints="breakpoints">
           <Slide v-for="(item, index) in developmentRoomImages" :key="index">
@@ -146,25 +198,36 @@ Api.fetchNewsIndex('guest_house_top').then((data) => {
           </Slide>
           <template #addons>
             <Navigation />
+            <Pagination />
           </template>
         </Carousel>
         <div class="text-sm mt-8">
-          <p class="font-bold">開発スペースを使って様々なイベントを開催しています。よろしければごらんください!</p>
-          <a href="https://coderdojo-hachinohe.aomori.jp/posts/43109806" target="_blank" class="text-teal-600 underline">
+          <p class="font-bold">
+            開発スペースを使って様々なイベントを開催しています。よろしければごらんください!
+          </p>
+          <a
+            href="https://coderdojo-hachinohe.aomori.jp/posts/43109806"
+            target="_blank"
+            class="text-teal-600 underline"
+          >
             海が見えるゲストハウスで美味しいコーヒーを飲みながら作品作り！第20回CoderDojo八戸
           </a>
         </div>
       </div>
     </section>
-    <section class="section">
+
+    <section class="section bg-slate-100">
       <h2 class="text-slate-700 text-4xl text-center">Bath & Amenities</h2>
-      <p class="text-base text-slate-700　text-center">バス&アメニティ</p>
+      <p class="text-base text-slate-700 text-center">バス&アメニティ</p>
       <p class="text-base mt-10">
-        お風呂・お手洗いはリフォーム済みで新品です。宿泊ルームと同じ一階にございますので、ご自由にご利用ください。<br>
+        お風呂・お手洗いはリフォーム済みで新品です。宿泊ルームと同じ一階にございますので、ご自由にご利用ください。<br />
       </p>
       <div class="text-sm mt-8">
         <p class="font-bold">ご用意のあるアメニティ</p>
-        <p>シャンプー / コンディショナー / ボディーソープ / 化粧水 /ドライヤー/ ミニヘアアイロン / 歯ブラシ</p>
+        <p>
+          シャンプー / コンディショナー / ボディーソープ / ドライヤー /
+          ミニヘアアイロン / 歯ブラシ
+        </p>
       </div>
       <div class="mt-16">
         <Carousel :settings="settings" :breakpoints="breakpoints">
@@ -180,10 +243,16 @@ Api.fetchNewsIndex('guest_house_top').then((data) => {
       </div>
     </section>
     <div class="section">
-      <h2 class="text-center text-slate-700 text-2xl font-bold py-16">お知らせ</h2>
-      <NewsList :newsList="news" :parentRoute="'guest_house'"/>
+      <h2 class="text-center text-slate-700 text-2xl font-bold py-16">
+        お知らせ
+      </h2>
+      <NewsList :newsList="news" :parentRoute="'guest_house'" />
       <div class="flex justify-center mt-10">
-        <a href="/guest_house/news" class="btn w-fit bg-teal-600 text-white px-6 py-3 text-sm">お知らせ一覧へ</a>
+        <a
+          href="/guest_house/news"
+          class="btn w-fit bg-teal-600 text-white px-6 py-3 text-sm"
+          >お知らせ一覧へ</a
+        >
       </div>
     </div>
   </main>
@@ -195,7 +264,7 @@ Api.fetchNewsIndex('guest_house_top').then((data) => {
   flex-direction: row-reverse;
   align-items: center;
   width: 100%;
-  background-image: url('/images/GH/kv.jpg');
+  background-image: url("/images/GH/kv.jpg");
   height: 700px;
   background-attachment: fixed;
   background-size: 100%;
@@ -241,7 +310,7 @@ Api.fetchNewsIndex('guest_house_top').then((data) => {
   &_title {
     position: relative;
     &::after {
-      content: '';
+      content: "";
       display: block;
       width: 80%;
       border-bottom: solid 1px black;
--- src/views/GuestHouse/Reserve.vue
+++ src/views/GuestHouse/Reserve.vue
@@ -1,36 +1,57 @@
 <script setup>
-import { ref } from 'vue'
-
+import { ref } from "vue";
 </script>
 <template>
   <section class="section mt-16">
     <h2 class="text-slate-700 text-4xl text-center">RESERVE</h2>
-    <p class="text-base text-slate-700　text-center">ご予約</p>
-    <h3 class="text-center text-lg font-bold text-red-600 mt-8">管理人の手が全く回っていないため、ご予約はお問い合わせでのみ行っておりますm(_ _)m</h3>
-    <!-- <p class="text-center text-sm font-bold mt-8">
-      
-    </p> -->
+    <p class="text-base text-slate-700 text-center">ご予約</p>
     <div class="text-center mt-16">
-      <!-- <span class="text-xs font-bold text-red-500">モニター価格</span><br> -->
       <span class="underline underline-offset-8 text-slate-700">
-        <span class="text-xl font-bold">料金:</span>
+        <span class="text-xl font-bold"
+          >大人<span class="text-ms font-bold">（中学生以上）</span
+          >:&nbsp;</span
+        >
         <span class="text-3xl font-bold">5,000</span>
-        <span class="text-sm font-bold">円(税込)</span>
+        <span class="text-sm font-bold">円(税込)/1泊</span>
       </span>
     </div>
-    <div class="text-center mt-1">
-      <span class="text-xs font-bold">お一人あたり / 一泊あたり / 最大4名様くらい(応相談)</span><br>
-      <span class="text-xs font-bold　text-slate-700">お支払いはPayPalでお願いしております</span><br>
+    <div class="text-center mt-8">
+      <span class="underline underline-offset-8 text-slate-700">
+        <span class="text-xl font-bold">小学生:&nbsp;</span>
+        <span class="text-3xl font-bold">3,000</span>
+        <span class="text-sm font-bold">円(税込)/1泊</span>
+      </span>
     </div>
+    <p class="mt-6 text-center text-xs font-bold">
+      お布団が必要ない未就学のお子様は無料です。
+    </p>
+    <p class="mt-1 text-center text-xs font-bold">
+      お支払いはPayPalでお願いしております。
+    </p>
+    <p class="mt-6 text-center text-sm font-bold mt-6">
+      1Fのゲストルームが最大４名様、2階の臨時ゲストルームは最大2名様（合計6名様）までご宿泊いただけます。
+    </p>
+
     <div class="bg-slate-200 px-10 py-6 mt-16">
-      <h3 class="text-center text-lg font-bold text-slate-700 underline">ご予約の手順</h3>
-       <p class="text-center text-lg font-bold text-red-600 mt-8">
-        申し訳ございませんが、<a href="/guest_house/contact" class="font-bold underline">お問い合わせフォーム</a>からご相談ください(*´･人･*)
-      </p>
-      <ol class="mt-6">
-        <li class="font-bold line-through ">1.<a href="#calendar" class="font-bold underline">宿泊カレンダー</a>で予約可能な日程の確認をお願いします。</li>
-        <li class="font-bold mt-2 line-through">2.希望の日程が「宿泊可能」となっていたら、下の<a href="#form" class="font-bold underline">申し込みフォーム</a>に記入の上送信をお願いします。</li>
-        <li class="font-bold mt-2 line-through">3.管理人から確認のメールを返信させていただき、メール上で予約確定となります。</li>
+      <h3 class="text-center text-lg font-bold text-slate-700 underline">
+        ご予約の手順
+      </h3>
+      <ol class="mt-12">
+        <li class="font-bold">
+          1.<a href="#calendar" class="font-bold underline">宿泊カレンダー</a
+          >上で「宿泊可能」と表示されている日程が予約可能な日程ですのでご確認ください。
+        </li>
+        <li class="font-bold mt-2">
+          2.希望の日程が「宿泊可能」となっていたら、下の<a
+            href="#form"
+            class="font-bold underline"
+            >申し込みフォーム</a
+          >に記入の上送信をお願いします。
+        </li>
+        <li class="font-bold mt-2">
+          3.Drop
+          inのメールアドレス（minato.dropin@gmail.com）から返信、またはお電話（070-6511-3600）させていただき、予約確定となります。
+        </li>
       </ol>
       <p class="text-sm font-bold text-red-600 mt-8">
         ・管理人の他に2歳の子供が同じ家で生活しております。ご了承ください。
@@ -39,22 +60,48 @@ import { ref } from 'vue'
         ・お食事のご用意はございませんので、自炊か外食でお願いしております。
       </p>
     </div>
-    <h2 class="mt-24 text-slate-700 text-xl font-bold text-center" id="calendar">宿泊カレンダー</h2>
+    <h2
+      class="mt-24 text-slate-700 text-xl font-bold text-center"
+      id="calendar"
+    >
+      宿泊カレンダー
+    </h2>
     <p class="text-sm font-bold mt-8">
-      現在こちらのカレンダーに「宿泊可能」は表示されません。<br>
-      「不在」以外の日程は個別で相談と言う形になります。お手数をおかけします(*´･人･*)<br><br>
-      また、こちらのカレンダー上で「開発スペース開放」となっている時間帯は二階の開発スペースを無料開放中です!<br>
-      <span>特にご予約不要でご自由にご利用いただけます!ゲストハウスを一緒に作りつつ、一緒にお仕事しませんか^^</span>
+      こちらのカレンダー上で「開発スペース開放」となっている時間帯は2階の開発スペースを無料開放中です!<br />
+      <span
+        >こちらは宿泊されない方でも、ご予約不要でコワーキングスペースとしてご自由にご利用いただけます!ゲストハウスを一緒に作りつつ、一緒にお仕事しませんか^^</span
+      >
     </p>
-    <p class="text-slate-700　text-sm mt-2">
-      詳しくは<a href ="https://dropin8.site/guest_house/news/g-3c7719r" class="underline">こちらの記事</a>をご覧ください♫
+    <p class="text-slate-700 text-sm mt-2">
+      詳しくは<a
+        href="https://dropin8.site/guest_house/news/g-3c7719r"
+        class="underline"
+        >こちらの記事</a
+      >をご覧ください♫
     </p>
-    <iframe class="w-full mt-6" src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=Asia%2FTokyo&showTitle=0&showCalendars=0&src=MjZiYjA4M2E1NGY0YTdlNzc5MjAyMjcxYmJmNmQ3NmRjYjFmMzNhODcwMjVlNjRkODk0OGNjMTkyMDA5ZTE3ZEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=NGJlNTYxYmYzY2M4OTRjZDI4YzI0MTNkNTQ5ODgzNDA4Yjg2NjIyYWUyNDE3MmNlNTZmMTU2ZDUyNjIyMjlkYkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=NTM1NmE5Njk3OWZmY2YwMTIxOTk0NzEzNzkwZWFlMTg1M2JhZTQzMjc3MjUwMWI3YmVjYTMyYWM0ZmRmMTE2NkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%233F51B5&color=%23B39DDB&color=%23c94c74" style="border-width:0" height="600" frameborder="0" scrolling="no"></iframe>
+    <iframe
+      class="w-full mt-6"
+      src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=Asia%2FTokyo&showTitle=0&showCalendars=0&src=MjZiYjA4M2E1NGY0YTdlNzc5MjAyMjcxYmJmNmQ3NmRjYjFmMzNhODcwMjVlNjRkODk0OGNjMTkyMDA5ZTE3ZEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=NGJlNTYxYmYzY2M4OTRjZDI4YzI0MTNkNTQ5ODgzNDA4Yjg2NjIyYWUyNDE3MmNlNTZmMTU2ZDUyNjIyMjlkYkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=NTM1NmE5Njk3OWZmY2YwMTIxOTk0NzEzNzkwZWFlMTg1M2JhZTQzMjc3MjUwMWI3YmVjYTMyYWM0ZmRmMTE2NkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&color=%233F51B5&color=%23B39DDB&color=%23c94c74"
+      style="border-width: 0"
+      height="600"
+      frameborder="0"
+      scrolling="no"
+    ></iframe>
   </section>
-  <!-- <div class="bg-slate-100">
+  <div class="bg-slate-100">
     <section class="section" id="form">
-      <h2 class="text-slate-700 font-bold text-xl text-center">申し込みフォーム</h2>
-      <iframe class="w-full" src="https://docs.google.com/forms/d/e/1FAIpQLSfVfmJqvZoKZa13vGnt2Ho6ZuWUvrQnGZVwQE69gbBxdSyZkw/viewform?embedded=true" height="1850" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
+      <h2 class="text-slate-700 font-bold text-xl text-center">
+        申し込みフォーム
+      </h2>
+      <iframe
+        class="w-full"
+        src="https://docs.google.com/forms/d/e/1FAIpQLSfVfmJqvZoKZa13vGnt2Ho6ZuWUvrQnGZVwQE69gbBxdSyZkw/viewform?embedded=true"
+        height="1850"
+        frameborder="0"
+        marginheight="0"
+        marginwidth="0"
+        >読み込んでいます…</iframe
+      >
     </section>
-  </div> -->
+  </div>
 </template>
