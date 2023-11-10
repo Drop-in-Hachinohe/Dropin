abe98016bf3c551edd74c6d0a481f601b3dcc944
yuta-sksn <sukekawa@thymefield.jp>
2023-11-11 01:14:20 +0900

    fix: サブドメインルーティングの不具合修正

--- app/router.options.ts
+++ app/router.options.ts
@@ -8,8 +8,9 @@ export default <RouterConfig> {
         // check for subdomain in the url on the server
         if (process.server && ssrContext && ssrContext.event.node.req) {
             const req = ssrContext.event.node.req
-            const subdomain = req.headers.host?.split('.')[0]
-            if (subdomain === 'space') {
+            const subdomainConcat = req.headers.host?.split('.').concat() as string[]
+            const subdomain = subdomainConcat.slice(0, subdomainConcat.length - 2).join('.')
+            if (subdomain !== 'guest-house' && subdomain !== 'dev.guest-house') {
                 routesDirectory = 'space'
             } else {
                 routesDirectory = 'guest-house'
@@ -19,8 +20,9 @@ export default <RouterConfig> {
 
         // check for subdomain in the url on the client
         if (process.client && window.location.hostname) {
-            const subdomain = window.location.hostname.split('.')[0]
-            if (subdomain === 'space') {
+            const subdomainConcat = window.location.hostname.split('.').concat()
+            const subdomain = subdomainConcat.slice(0, subdomainConcat.length - 2).join('.')
+            if (subdomain !== 'guest-house' && subdomain !== 'dev.guest-house') {
                 routesDirectory = 'space'
             } else {
                 routesDirectory = 'guest-house'
